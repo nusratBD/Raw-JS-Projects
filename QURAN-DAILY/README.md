7184dc190d742da3448ef5134875a056a7f3d30a
nusratBD <nusrat.hurain15@gmail.com>
2025-05-05 21:51:01 +0600

    Surah List and Detail of Each Surah by clicking on the Surah's Name

---
+++ QURAN-DAILY/README.md
@@ -0,0 +1,14 @@
+# 📖 Quran Daily
+Quran Daily is a simple web application that lists all Surahs (chapters) of the Quran and allows users to view detailed information by clicking on each Surah.
+
+## 🚀 Features
+- ✅ Display a list of all Surahs from the Quran
+- ✅ Show details of each Surah on click
+- (More features coming soon...)
+
+
+## 🛠️ Technologies Used
+
+- ✅ HTML
+- ✅ CSS
+- ✅ JavaScript (Vanilla)
---
+++ QURAN-DAILY/script.js
@@ -0,0 +1,30 @@
+//Show All Surahs
+document.addEventListener("DOMContentLoaded",()=>{
+    fetch(`https://quranapi.pages.dev/api/surah.json`)
+    .then(res=>res.json())
+    .then(data=>{console.log(data);
+        const surahList=document.getElementById("surahList");
+        data.forEach((surah,index) => {
+            surahList.innerHTML+=`
+                <button style="margin:2px" onclick="searchSurah(${index+1})">${index+1}. ${surah.surahName}</button>
+            `;
+        });
+    });
+})
+//Show Specific Surah
+const surahName=document.getElementById("surahName");
+const surahContainer=document.getElementById("surahContainer");
+const searchSurah=(surahNumber)=>{
+fetch(`https://quranapi.pages.dev/api/${surahNumber}.json`)
+.then(res=>res.json())
+.then(data=>{
+    console.log(data);
+    surahName.textContent=`${surahNumber}. ${data.surahName}`;
+    data.arabic1.forEach((arabic,index)=>{
+        surahContainer.innerHTML+=`
+        <p><strong>${index+1}.</strong> ${arabic}<p>
+        <p> ${data.english[index]}<p>
+        `;
+    })
+});
+}
